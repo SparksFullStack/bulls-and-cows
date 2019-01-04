b6b356b5b3b1a898418cbb1366172a455f7bcece
SparksFullStack <csparks@antioch.edu>
2019-01-04 13:10:13 -0800

    added functionality that allows the user to play again

--- bulls_and_cows.cpp
+++ bulls_and_cows.cpp
@@ -4,20 +4,17 @@
 using namespace std;
 
 void PrintIntro();
+void PlayGame();
 string GetGuess();
+bool AskToPlayAgain();
 
 int main()
 {
-  PrintIntro();
-
-  constexpr int NUMBER_OF_TURNS = 5;
-  for (int guessNum = 1; guessNum <= NUMBER_OF_TURNS; guessNum++)
-  {
-    string Guess = GetGuess();
-    cout << "Your guess was: " << Guess << endl;
-    cout << endl;
-  }
-
+  do {
+    PrintIntro();
+    PlayGame();
+  } while (AskToPlayAgain() == true);
+  
   return 0;
 }
 
@@ -29,6 +26,17 @@ void PrintIntro()
   cout << " letters long..." << endl;
 };
 
+void PlayGame()
+{
+  constexpr int NUMBER_OF_TURNS = 5;
+  for (int guessNum = 1; guessNum <= NUMBER_OF_TURNS; guessNum++)
+  {
+    string Guess = GetGuess();
+    cout << "Your guess was: " << Guess << endl;
+    cout << endl;
+  }
+}
+
 string GetGuess()
 {
   string Guess = "";
@@ -36,3 +44,12 @@ string GetGuess()
 
   return Guess;
 };
+
+bool AskToPlayAgain()
+{
+  cout << "Would you like to play again? (yes/no)" << endl;
+  string Response = "";
+  getline(cin, Response);
+
+  return (Response[0] == 'y') || (Response[0] == 'Y');
+}
