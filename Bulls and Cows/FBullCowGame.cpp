42236e51c3a86c68ab337f29b5abce830950ca0e
SparksFullStack <csparks@antioch.edu>
2019-01-07 13:06:16 -0800

    Added new header files

---
+++ Bulls and Cows/FBullCowGame.cpp
@@ -0,0 +1,9 @@
+//
+//  FBullCowGame.cpp
+//  Bulls and Cows
+//
+//  Created by Charlie Sparks on 1/7/19.
+//  Copyright © 2019 Charlie Sparks. All rights reserved.
+//
+
+#include "FBullCowGame.hpp"
---
+++ Bulls and Cows/FBullCowGame.hpp
@@ -0,0 +1,14 @@
+//
+//  FBullCowGame.hpp
+//  Bulls and Cows
+//
+//  Created by Charlie Sparks on 1/7/19.
+//  Copyright © 2019 Charlie Sparks. All rights reserved.
+//
+
+#ifndef FBullCowGame_hpp
+#define FBullCowGame_hpp
+
+#include <stdio.h>
+
+#endif /* FBullCowGame_hpp */
